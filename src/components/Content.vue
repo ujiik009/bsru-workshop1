5868bb2a3b2031ac68ce61e3bad1986bb9a9cdf3
LDC.ICE <ikool009@hotmail.com>
2021-11-20 14:18:36 +0700

    method

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h2>Content</h2>
-    <div>{{ Number(num1) + Number(num2) }}</div>
+
+    <!-- <div>{{ Number(num1) + Number(num2) }}</div>
     <div>
       First name : {{ info.first_name }} <br />
       Last Name : {{ info.last_name }}
@@ -10,34 +11,58 @@
     <div>
       <input v-model="num1" />
       <input v-model="num2" />
-    </div>
-    <h1>v-text and v-html</h1>
+    </div> -->
+    <!-- <h1>v-text and v-html</h1> -->
     <!-- v-text -->
-    <div v-text="username"></div>
-    == {{ username }}
+    <!-- <div v-text="username"></div>
+    == {{ username }} -->
 
     <!-- v-html -->
-    <div v-html="html_format"></div>
+    <!-- <div v-html="html_format"></div> -->
 
-    <h1>v-show and v-if</h1>
+    <!-- <h1>v-show and v-if</h1>
     <div v-show="hiden == true">v-show Example</div>
-    <div v-if="hiden == true">v-if Example</div>
+    <div v-if="hiden == true">v-if Example</div> -->
 
-    <h1>v-if and v-else</h1>
+    <!-- <h1>v-if and v-else</h1>
     <div v-if="number1 % 2 == 0">even</div>
-    <div v-else>odd</div>
+    <div v-else>odd</div> -->
 
-    <h1> v-for </h1>
+    <!-- <h1> v-for </h1>
     <div class="item" v-for="(car_item , index) in cars" :key="index">
       Car Name : {{car_item.name}} <br/>
       Car Color : {{car_item.color}}
+    </div> -->
+
+    <!-- <h1>v-on</h1>
+    <div>
+      Your Counter {{ counter }}
+  
+      <button @click="counter += 1">+</button>
+
+      <button @click="counter -= 1">-</button>
+
+      <select @change="counter += 1">
+        <option value="green">green</option>
+        <option value="blue">blue</option>
+      </select>
+    </div> -->
+
+    <h1>Method</h1>
+    <div>
+      {{username}} : {{password}}
+      <br/>
+      <input type="text" v-model="username">
+      <br/>
+      <input type="password" v-model="password">
+      <br/>
+      <button @click="show_alert('Ice')"> Show Alert </button>
     </div>
+
     <Avatar />
   </div>
 </template>
-
 <script>
-
 import Avatar from "@/components/Avatar.vue";
 export default {
   components: {
@@ -45,6 +70,7 @@ export default {
   },
   data() {
     return {
+      counter: 0,
       number1: 2,
       hiden: true,
       username: "Apirat",
@@ -68,11 +94,16 @@ export default {
       ],
     };
   },
+  methods:{
+    show_alert(nickname){
+      alert(this.username+":"+this.password+":"+nickname)
+    }
+  }
 };
 </script>
 
 <style scoped>
-.item{
+.item {
   background-color: tomato;
   margin-bottom: 10px;
 }
